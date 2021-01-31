24481e8468c54f588f65a4b86d9cf9b07a0d31c7
BT <bunyamintamar@yandex.com>
2021-01-31 22:55:44 +0300

    [+] Calculate fibonacci number

--- main.cpp
+++ main.cpp
@@ -1,45 +1,29 @@
+/* C program for Tabulated version */
 #include <iostream>
-#include <math.h>
 
 using namespace std;
 
-void Get_Roots(float a, float b, float c, float *r1, float *r2)
+int fib(int n) 
+{ 
+  int f[n+1]; 
+  int i; 
+  f[0] = 0;
+  f[1] = 1;
+  
+  for (i = 2; i <= n; i++)
+  {
+      f[i] = f[i-1] + f[i-2];
+  }
+  
+  return f[n]; 
+} 
+   
+int main () 
 {
-	float delta = (float)pow((double)b,2) - 4 * a * c;
-	
-	if( delta > 0 )
-	{
-		*r1 = ( -b + (float)sqrt((double)delta) ) / (2*a);
-		*r2 = ( -b - (float)sqrt((double)delta) ) / (2*a);
-		cout << "Different roots" << endl;
-	}
-	else if( delta == 0 )
-	{
-		*r1 = -b / (2*a);
-		*r2 = *r1;
-		cout << "Double roots" << endl;
-	}
-	else
-	{
-		cout << "No real roots" << endl;
-	}
+	int n;
+  cout << "Enter a number for fibonacci : ";
+  cin >> n;
+  cout << "Fibonacci number             : " << fib(n);
+  
+  return 0; 
 }
-
-int main(void)
-{
-	float a,b,c,r1,r2;
-	
-	cout << "aX^2 + bX + c" << endl;
-	
-	cout << "a : ";
-	cin >> a;
-	cout << "b : ";
-	cin >> b;
-	cout << "c : ";
-	cin >> c;
-	
-	Get_Roots(a,b,c,&r1,&r2);
-	cout << "r1 : " << r1 << endl << "r2 : " << r2;
-	return 0;
-}
-
