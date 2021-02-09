eab8874b46867befba10b68c7dbb7fc42cfb8d40
BT <bunyamintamar@yandex.com>
2021-02-09 23:09:40 +0300

    [*] LinkedList in C has been updated

--- main.c
+++ main.c
@@ -1,185 +1,76 @@
-/*
-File name: test.c
-Compile: cc test.c
-*/
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node{
-    int val;
+struct SinglyLinkedList
+{
+    int data;
     struct node *next;
 };
 
-void print_list(struct node *head)
+
+/**
+ *	print_list function
+ *	@param	: *list		<	pointer of the root list
+ *	@return	: none
+ */
+void print_list(struct SinglyLinkedList *list)
 {
     printf("H->");
 
-    while(head)
+    while(list)							/*!< WHILE list pointer is NOT NULL */
     {
-        printf("%d->", head->val);
-        head = head->next;
+        printf("%d->", list->data);		/*!< printf data */
+        list = list->next;				/*!< next pointer will be current pointer */
     }
 
     printf("|||\n\n");
 }
 
-void insert_front(struct node **head, int value)
+
+/**
+ *	Insert new element at the end of the linked list
+ *	@param	: **list	< pointer of pointer of the linked list
+ *	@param	: value		< new element
+ */
+void insert_end(struct node **list, int value)
 {
-    struct node * new_node = NULL;
+    struct SinglyLinkedList *new_node	= NULL;										/*!< pointer of new node */
+    struct SinglyLinkedList *last		= NULL;										/*!< pointer of the last node */
 
-    new_node = (struct node *)malloc(sizeof(struct node));
+    new_node = (struct SinglyLinkedList *)malloc(sizeof(struct SinglyLinkedList));	/*!< memory allocation for new node */
 
-    if (new_node == NULL)
+    if (new_node == NULL)															/*!< Check the memory */
     {
-        printf("Failed to insert element. Out of memory");
+        printf("Failed to insert element. Out of memory");							/*!< Alert for out of memory */
     }
 
-    new_node->val = value;
-    new_node->next = *head;
-
-    *head = new_node;
-}
-
-void insert_end(struct node **head, int value)
-{
-    struct node * new_node = NULL;
-    struct node * last = NULL;
-
-    new_node = (struct node *)malloc(sizeof(struct node));
-
-    if (new_node == NULL)
-    {
-        printf("Failed to insert element. Out of memory");
-    }
-
-    new_node->val = value;
+    new_node->data = value;															/*!< value to new_node->data */
     new_node->next = NULL;
 
-    if( *head == NULL)
+    if( *list == NULL)																/*!< Check the list if it is NULL */
     {
-        *head = new_node;
+        *list = new_node;
         return;
     }
-
-    last = *head;
+    
+    /* NOT NULL */
+    last = *list;																	/*!< pointer of the root */
+    
+    /* if last->next is NULL, last is the real last */
     while(last->next) last = last->next;
 
-    last->next = new_node;
-}
-
-void insert_after(struct node *head, int value, int after)
-{
-    struct node * new_node = NULL;
-    struct node *tmp = head;
-
-    while(tmp) {
-
-      if(tmp->val == after) {  /*found the node*/
-
-        new_node = (struct node *)malloc(sizeof(struct node));
-
-        if (new_node == NULL) {
-          printf("Failed to insert element. Out of memory");
-        }
-
-        new_node->val = value;
-        new_node->next = tmp->next;
-        tmp->next = new_node;
-        return;
-      }
-
-      tmp = tmp->next;
-    }
-}
-
-void insert_before(struct node **head, int value, int before)
-{
-    struct node * new_node = NULL;
-    struct node * tmp = *head;
-
-    new_node = (struct node *)malloc(sizeof(struct node));
-
-    if (new_node == NULL)
-    {
-        printf("Failed to insert element. Out of memory");
-        return;
-    }
-
-    new_node->val = value;
-
-    if((*head)->val == before)
-    {
-        new_node->next = *head;
-        *head = new_node;
-        return;
-    }
-
-    while(tmp && tmp->next) {
-
-      if(tmp->next->val == before) {
-        new_node->next = tmp->next;
-        tmp->next = new_node;
-        return;
-      }
-
-      tmp = tmp->next;
-    }
-
-    /*Before node not found*/
-    free(new_node);
+    last->next = new_node;															/*!< new_node is last node */
 }
 
 void main()
 {
-    int count = 0, i, val, after, before;
-    struct node * head = NULL;
+    struct SinglyLinkedList * LinkedList = NULL;
 
-    printf("Enter number of elements: ");
-    scanf("%d", &count);
-
-    for (i = 0; i < count; i++)
-    {
-        printf("Enter %dth element: ", i);
-        scanf("%d", &val);
-        insert_front(&head, val);
-    }
-
-    printf("Initial List: ");
-    print_list(head);
-
-    printf("Enter a value to enter at the front of the list: ");
-    scanf("%d", &val);
-    insert_front(&head, val);
+    insert_end(&LinkedList, 10);
+    insert_end(&LinkedList, 20);
+    insert_end(&LinkedList, 30);
+    insert_end(&LinkedList, 40);
 
     printf("List after insertion: ");
-    print_list(head);
-
-    printf("Enter a value to enter at the end of the list: ");
-    scanf("%d", &val);
-    insert_end(&head, val);
-
-    printf("List after insertion: ");
-    print_list(head);
-
-    printf("Enter a value to insert in the list: ");
-    scanf("%d", &val);
-
-    printf("Insert after: ");
-    scanf("%d", &after);
-
-    insert_after(head, val, after);
-
-    printf("List after insertion: ");
-    print_list(head);
-
-    printf("Enter a value to insert in the list: ");
-    scanf("%d", &val);
-
-    printf("Insert before: ");
-    scanf("%d", &before);
-
-    insert_before(&head, val, before);
-
-    printf("List after insertion: ");
-    print_list(head);
+    print_list(LinkedList);
 }
